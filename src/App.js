17a680dce661ed094b2285fc8e981dacfdd4682a
WinstonLinCanada <liu31812202@hotmail.com>
2018-07-30 01:03:56 -0700

    miao

--- src/App.js
+++ src/App.js
@@ -11,7 +11,13 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload. Niconiconi! make it!
+        </p>
+        <p className="App-intro">
+          Did a good job
+        </p>
+        <p className="App-intro">
+          Make it happen!
         </p>
       </div>
     );
