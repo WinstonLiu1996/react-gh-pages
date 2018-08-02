e6f45c64b7666e8189dd32fedbd46cad9a888a22
WinstonLinCanada <liu31812202@hotmail.com>
2018-08-01 20:04:16 -0700

    Fix homepage url

--- src/App.js
+++ src/App.js
@@ -11,13 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload. Niconiconi! make it!
-        </p>
-        <p className="App-intro">
-          Did a good job
-        </p>
-        <p className="App-intro">
-          Make it happen!
+          edit <code>src/App.js</code> and save to reload. Niconiconi! make it!
         </p>
       </div>
     );
